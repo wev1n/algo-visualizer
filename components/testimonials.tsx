3156c5242fbe3428a68eec3ee530c51bbb2793cb
wevin <kevin.warme@gmail.com>
2024-07-14 18:03:30 +0200

    Add skeleton for testimonials on landingPage

--- components/testimonials.tsx
+++ components/testimonials.tsx
@@ -1,6 +1,6 @@
-import * as React from "react";
+import Image from "next/image";
+import { Star } from "lucide-react";
 
-import { Card, CardContent } from "@/components/ui/card";
 import {
   Carousel,
   CarouselContent,
@@ -8,20 +8,55 @@ import {
   CarouselNext,
   CarouselPrevious,
 } from "@/components/ui/carousel";
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+import { Separator } from "./ui/separator";
 
 export default function Testimonials() {
   return (
     <section className="flex justify-center items-center p-20">
-      <Carousel className="w-full max-w-xs">
+      <Carousel className="w-full max-w-6xl">
         <CarouselContent>
           {Array.from({ length: 5 }).map((_, index) => (
             <CarouselItem key={index}>
-              <div className="p-1">
-                <Card>
-                  <CardContent className="flex aspect-square items-center justify-center p-6">
-                    <span className="text-4xl font-semibold">{index + 1}</span>
-                  </CardContent>
-                </Card>
+              <div className="flex flex-col gap-6 items-center">
+                <div className="flex gap-2">
+                  <Star className="text-yellow-500 h-7 w-7" />
+                  <Star className="text-yellow-500 h-7 w-7" />
+                  <Star className="text-yellow-500 h-7 w-7" />
+                  <Star className="text-yellow-500 h-7 w-7" />
+                  <Star className="h-7 w-7" />
+                </div>
+                <h4 className="text-2xl font-bold text-center">
+                  The website has been an invaluable resource for enhancing my
+                  learning experience. The visualization of algorithms and data
+                  structures has made complex concepts much easier to
+                  understand.
+                </h4>
+                <div className="flex justify-center items-center gap-6">
+                  <Avatar>
+                    <AvatarImage
+                      src="https://github.com/shadcn.png"
+                      alt="@shadcn"
+                    />
+                    <AvatarFallback>CN</AvatarFallback>
+                  </Avatar>
+                  <div>
+                    <h6 className="font-semibold">John Doe {index + 1}</h6>
+                    <span className="text-sm">Software Engineer, ABC Corp</span>
+                  </div>
+
+                  <Separator orientation="vertical" className="h-20 mx-4" />
+
+                  <div className="flex items-center gap-4">
+                    <Image
+                      src="/github.svg"
+                      alt="hello"
+                      width={40}
+                      height={40}
+                    />
+                    <span className="text-lg">GitHub</span>
+                  </div>
+                </div>
               </div>
             </CarouselItem>
           ))}
